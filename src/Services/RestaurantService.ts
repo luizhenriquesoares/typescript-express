6381531f2da8f96a24ef6df5fea49c872e387991
luiz henrique soares <luizhenriquesoares91@gmail.com>
2019-01-30 23:20:31 -0300

    clearning code services

--- src/Services/RestaurantService.ts
+++ src/Services/RestaurantService.ts
@@ -4,20 +4,15 @@ import { RestaurantRepository } from '../Insfrastructure/Repositories/Restaurant
 import IRestauranteService from './Interfaces/IRestauranteService';
 import 'reflect-metadata';
 import { Restaurant } from '../Domains/Restaurant/Restaurant';
-import { ModelType } from 'typegoose';
 import { TYPES } from '../Insfrastructure/CrossCutting/DI/Types';
-import { schemaOptions, BaseDomain } from '../Domains/BaseDomain';
+import { BaseDomain } from '../Domains/BaseDomain';
 
-// implements IRestauranteService<Restaurant>
 @injectable()
 export class RestaurantService extends BaseService<Restaurant> implements IRestauranteService<BaseDomain> {
 
-    protected _DOMAIN: Restaurant;
-
     constructor(@inject(TYPES.IBaseRepository) repository: RestaurantRepository, @inject(TYPES.IRestaurante) restaurantDomain: Restaurant) {
         super(repository);
-        this._DOMAIN = restaurantDomain;
-        this._MODEL = this._DOMAIN.model();
+        this._MODEL = restaurantDomain.model();
     }
 
      public getRestaurante(){
